6449ee520544ae1d0d2e91e0c49d64d56e136349
mqsilveira <marcosilveira.lg@gmail.com>
2023-12-19 21:25:05 -0300

    atualizando configurações do banco

--- src/config.js
+++ src/config.js
@@ -1,10 +1,18 @@
+require('dotenv').config();
+
 module.exports = {
     development: {
-        username: "postgres",
-        password: "postgres",
-        database: "postgres",
-        host: "localhost",
-        dialect: "postgres",
-        port: 5435
-    }
-};
+        username: process.env.USER,
+        password: process.env.PASSWORD,
+        database: process.env.DATABASE_URL,
+        host: process.env.HOST,
+        dialect: process.env.DIALET,
+        timezone: '-03:00',
+        port: process.env.PORT,
+        define: {
+            underscored: true,
+            timestamps: false
+        }
+    },
+    secret: process.env.SECRET
+}
